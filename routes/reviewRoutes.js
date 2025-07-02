430d241a7b095c31c3774291317c70cf7bd725f1
shivamchoudhary15 <sc0626110@gmail.com>
2025-07-02 15:01:39 +0530

    Update reviewRoutes.js

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Review = require('../models/review');
+const Review = require('../models/Review');
 
 // Submit a new review
 router.post('/submit', async (req, res) => {
