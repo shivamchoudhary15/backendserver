5ffc753aaf70067faa21698ff1f512a350bfe2f1
shivamchoudhary15 <sc0626110@gmail.com>
2025-07-07 12:31:58 +0530

    Update Review.js

--- models/Review.js
+++ models/Review.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const ReviewSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  rating: { type: Number, min: 1, max: 5, required: true },
-  comment: { type: String, required: true },
-  review_date: { type: Date, default: Date.now }
+  name:{type:String,required:true },
+  rating:{type:Number,min:1,max:5,required: true },
+  comment:{type:String, required: true },
+  review_date:{type: Date,default: Date.now }
 });
 
 module.exports = mongoose.model('Review', ReviewSchema);
