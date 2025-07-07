fe6c10a048c7a9c1a4dbc08be275c0a084c661af
shivamchoudhary15 <sc0626110@gmail.com>
2025-07-07 12:57:59 +0530

    Update ProtectedRoute.js

--- pandit/src/components/ProtectedRoute.js
+++ pandit/src/components/ProtectedRoute.js
@@ -1,15 +1,15 @@
-// src/components/ProtectedRoute.js
+// ye unathorised user ko prevent karega or navigate kareya home page jab user ke pass token nahi hai 
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import {Navigate} from 'react-router-dom';
 
 export default function ProtectedRoute({ children }) {
   const token = localStorage.getItem('token');
 
   if (!token) {
-    // If no token, redirect to home
+    // yaa se redirect ho jayega 
     return <Navigate to="/home" replace />;
   }
 
-  // If token exists, render the protected component
+ 
   return children;
 }
