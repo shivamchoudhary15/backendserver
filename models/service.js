6182cd8845d0584714d15f500ddaa5aa58e681a8
shivamchoudhary15 <sc0626110@gmail.com>
2025-07-09 20:36:09 +0530

    Update service.js

--- models/service.js
+++ models/service.js
@@ -1,10 +1,12 @@
-const mongoose=require('mongoose');
+// models/service.js
+const mongoose = require('mongoose');
 
-const Serviceschema=new mongoose.Schema({
+const Serviceschema = new mongoose.Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
-  name:{type:String,required:true },
-  description:{type:String },
-  price:{ type:Number, required:true }
+  name: { type: String, required: true },
+  description: { type: String },
+  price: { type: Number, required: true },
+  image: { type: String }, 
 });
 
 module.exports = mongoose.model('Service', Serviceschema);
