7ccd1c6056d02eebd5a5a7653b13353ca8d4b8ac
shivamchoudhary15 <sc0626110@gmail.com>
2025-07-12 11:06:20 +0530

    Update pandit.js

--- models/pandit.js
+++ models/pandit.js
@@ -1,17 +1,23 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const Panditschema=new mongoose.Schema({
+const Panditschema = new mongoose.Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
-  name:{type:String , required:true},
-  phone:{type:String , required:true, unique:true},
-  email:{type:String, required:true, unique:true},
-  city:{type:String},
-  experienceYears:{type:Number},
-  languages:{type:String},
-  specialties:{type:String},
-  bio:{type:String},
-  profile_photo_url:{type:String},
-  is_verified:{type:Boolean,default:false},
-  password:{type:String,required:true,min:8,max:8},
+  name: { type: String, required: true },
+  phone: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  city: { type: String },
+  experienceYears: { type: Number },
+  languages: { type: String },
+  specialties: { type: String },
+  bio: { type: String },
+  profile_photo_url: { type: String },
+  is_verified: { type: Boolean, default: false },
+  password: { type: String, required: true, min: 8, max: 8 },
+  role: {
+    type: String,
+    enum: ['pandit'],
+    default: 'pandit',
+  }
 });
-module.exports=mongoose.model('Pandit',Panditschema);
+
+module.exports = mongoose.model('Pandit', Panditschema);
