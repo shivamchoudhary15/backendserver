8352dc9c29dea43b198211afa69fa6488b310518
shivamchoudhary15 <sc0626110@gmail.com>
2025-07-12 13:36:16 +0530

    Update AdminProtectedRoute.js

--- pandit/src/components/AdminProtectedRoute.js
+++ pandit/src/components/AdminProtectedRoute.js
@@ -1,12 +1,18 @@
-// src/components/AdminProtectedRoute.js
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const AdminProtectedRoute = ({ children }) => {
   const token = localStorage.getItem('token');
-  const user = JSON.parse(localStorage.getItem('user'));
+  let user;
+
+  try {
+    user = JSON.parse(localStorage.getItem('user'));
+  } catch (err) {
+    user = null;
+  }
 
   if (!token || !user || user.role !== 'admin') {
+    alert("You are not authorized to access the admin panel.");
     return <Navigate to="/login" replace />;
   }
 
