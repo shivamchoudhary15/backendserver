ad7b95e02d5ac023891f8a9b2a5cd7edc772a634
shivamchoudhary15 <sc0626110@gmail.com>
2025-07-12 10:20:27 +0530

    Update booking.js

--- models/booking.js
+++ models/booking.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose');
 
 const BookingSchema = new mongoose.Schema({
-  userid:{type:String,required: true },      
-  panditid:{type:String,required: true },     
-  serviceid:{type:String,required: true },    
-  puja_date:{type:Date,required: true },
-  puja_time:{type:String},
-  location:{type:String },
-  SamanList:{type:String },
-  status:{type:String,default: 'Pending' },
-  created_at:{type: Date,default: Date.now }
+  userid: { type: String, required: true },
+  panditid: { type: String, required: true },
+  serviceid: { type: String, required: true },
+  puja_date: { type: Date, required: true },
+  puja_time: { type: String },
+  location: { type: String },
+  SamanList: { type: String },
+  status: { type: String, default: 'Pending' }, // Pending, Accepted, Rejected
+  created_at: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Booking', BookingSchema);
-
