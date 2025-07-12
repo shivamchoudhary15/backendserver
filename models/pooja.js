b9835493f0a011ecdef7b1b6d4906c021663e647
shivamchoudhary15 <sc0626110@gmail.com>
2025-07-12 18:58:33 +0530

    Update pooja.js

--- models/pooja.js
+++ models/pooja.js
@@ -4,6 +4,7 @@ const PoojaSchema = new mongoose.Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
   name: { type: String, required: true },
   description: { type: String },
+  imageUrl: { type: String }, // ✅ Added for image support
   itemsRequired: { type: [String] }, // optional list
 });
 
