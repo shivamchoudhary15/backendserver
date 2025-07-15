c95d9bfb3e9ba0d8c07bbbbc5696685026c689cb
shivamchoudhary15 <sc0626110@gmail.com>
2025-07-15 19:18:22 +0530

    Update service.js

--- models/service.js
+++ models/service.js
@@ -5,7 +5,7 @@ const Serviceschema = new mongoose.Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
   name: { type: String, required: true },
   description: { type: String },
-  price: { type: Number, required: true },
+  price: { type: String, required: true },
   image: { type: String }, 
 });
 
