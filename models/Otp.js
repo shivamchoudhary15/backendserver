79f1bcbab09b9d0fc11f0b4a8c352dcd85fb6861
shivamchoudhary15 <sc0626110@gmail.com>
2025-07-18 14:51:21 +0530

    Create Otp.js

---
+++ models/Otp.js
@@ -0,0 +1,10 @@
+// models/Otp.js
+const mongoose = require('mongoose');
+
+const OtpSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  otp: { type: String, required: true },
+  expiresAt: { type: Date, required: true }
+});
+
+module.exports = mongoose.model('Otp', OtpSchema);
