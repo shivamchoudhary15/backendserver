f25785c086249f705b3659b8a581227faebf1d33
shivamchoudhary15 <sc0626110@gmail.com>
2025-07-21 18:57:26 +0530

    Update Location.js

--- models/Location.js
+++ models/Location.js
@@ -1,11 +1,23 @@
-// models/Location.js
 const mongoose = require('mongoose');
 
 const locationSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Optional
-  latitude: { type: Number, required: true },
-  longitude: { type: Number, required: true },
-  timestamp: { type: Date, default: Date.now }
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: false //
+  },
+  latitude: {
+    type: Number,
+    required: true
+  },
+  longitude: {
+    type: Number,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('Location', locationSchema);
