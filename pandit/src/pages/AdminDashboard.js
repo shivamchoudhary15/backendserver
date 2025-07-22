0f1f9dc45eefe650f6bc96cc49677e1aefeaa7da
shivamchoudhary15 <sc0626110@gmail.com>
2025-07-22 19:00:06 +0530

    Update AdminDashboard.js

--- pandit/src/pages/AdminDashboard.js
+++ pandit/src/pages/AdminDashboard.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import DashboardLayout from '../components/DashboardLayout';
 import Home from './Home';
-import PanditsPage from './PanditsPage'; // Your existing Pandits list page component
-import DevoteesPage from './DevoteesPage'; // Your existing Devotees list page component
+import PanditsPage from './PanditsPage';
+import DevoteesPage from './DevoteesPage';
 import BookingHistory from './BookingHistory';
 import MapView from './MapView';
 
@@ -12,13 +12,23 @@ function AdminDashboard({ userLocations = [], panditLocations = [] }) {
     <Router>
       <Routes>
         <Route path="/admin" element={<DashboardLayout />}>
+          {/* Redirect index /admin path to /admin/home */}
+          <Route index element={<Navigate to="home" replace />} />
+
           <Route path="home" element={<Home />} />
           <Route path="pandits" element={<PanditsPage />} />
           <Route path="devotees" element={<DevoteesPage />} />
           <Route path="bookings" element={<BookingHistory />} />
-          <Route path="map" element={<MapView userLocations={userLocations} panditLocations={panditLocations} />} />
-          <Route index element={<Navigate to="home" replace />} />
+          <Route
+            path="map"
+            element={
+              <MapView userLocations={userLocations} panditLocations={panditLocations} />
+            }
+          />
         </Route>
+
+        {/* Optional: redirect any unknown routes to admin home */}
+        <Route path="*" element={<Navigate to="/admin/home" replace />} />
       </Routes>
     </Router>
   );
