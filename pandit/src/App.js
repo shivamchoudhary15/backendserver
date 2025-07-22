22f49d6f817bfec30077db6938b7e3234165a46e
shivamchoudhary15 <sc0626110@gmail.com>
2025-07-22 19:10:14 +0530

    Update App.js

--- pandit/src/App.js
+++ pandit/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Signup from './pages/Signup';
@@ -12,29 +12,27 @@ import Notifications from './pages/Notifications';
 import Dashboard from './pages/Dashboard';
 import PanditSignup from './pages/PanditSignup';
 import PanditDashboard from './pages/PanditDashboard';
-import AdminDashboard from './pages/AdminDashboard';
+import AdminDashboard from './pages/AdminDashboard';  // AdminDashboard as nested routes component
 import AdminLogin from './pages/AdminLogin';
 
-
 import ProtectedRoute from './components/ProtectedRoute';
-import AdminProtectedRoute from './components/AdminProtectedRoute'; // <-- New import
+import AdminProtectedRoute from './components/AdminProtectedRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signup/pandit" element={<PanditSignup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/pandit-dashboard" element={<PanditDashboard />} />
         <Route path="/admin-login" element={<AdminLogin />} />
 
-
-        {/* Role-protected admin route */}
+        {/* Admin routes wrapped in protection */}
         <Route
-          path="/admin"
+          path="/admin/*"
           element={
             <AdminProtectedRoute>
               <AdminDashboard />
@@ -42,7 +40,7 @@ function App() {
           }
         />
 
-        {/* Auth-protected routes */}
+        {/* Auth-protected user routes */}
         <Route
           path="/dashboard"
           element={
@@ -83,6 +81,9 @@ function App() {
             </ProtectedRoute>
           }
         />
+
+        {/* Catch-all redirect unknown routes to home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
