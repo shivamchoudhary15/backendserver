445378a2844619c30738b05cd12fed4c28ac09e2
ShyamSundar19193 <shyamsundar827394@gmail.com>
2025-07-22 12:43:09 +0530

    Update Review.js

--- models/Review.js
+++ models/Review.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const ReviewSchema = new mongoose.Schema({
-  
+  name:{type:String,required:true },
+  rating:{type:Number,min:1,max:5,required: true },
   comment:{type:String, required: true },
   review_date:{type: Date,default: Date.now }
 });
