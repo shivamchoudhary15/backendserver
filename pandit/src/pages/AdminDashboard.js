84c137bbc073c78ca0482404cf8294e8476dfcbc
shivamchoudhary15 <sc0626110@gmail.com>
2025-07-22 19:10:38 +0530

    Update AdminDashboard.js

--- pandit/src/pages/AdminDashboard.js
+++ pandit/src/pages/AdminDashboard.js
@@ -1,5 +1,7 @@
+// src/pages/AdminDashboard.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import DashboardLayout from '../components/DashboardLayout';
 import Home from './Home';
 import PanditsPage from './PanditsPage';
@@ -9,28 +11,24 @@ import MapView from './MapView';
 
 function AdminDashboard({ userLocations = [], panditLocations = [] }) {
   return (
-    <Router>
-      <Routes>
-        <Route path="/admin" element={<DashboardLayout />}>
-          {/* Redirect index /admin path to /admin/home */}
-          <Route index element={<Navigate to="home" replace />} />
+    <Routes>
+      <Route path="/" element={<DashboardLayout />}>
+        {/* Redirect /admin to /admin/home */}
+        <Route index element={<Navigate to="home" replace />} />
 
-          <Route path="home" element={<Home />} />
-          <Route path="pandits" element={<PanditsPage />} />
-          <Route path="devotees" element={<DevoteesPage />} />
-          <Route path="bookings" element={<BookingHistory />} />
-          <Route
-            path="map"
-            element={
-              <MapView userLocations={userLocations} panditLocations={panditLocations} />
-            }
-          />
-        </Route>
+        <Route path="home" element={<Home />} />
+        <Route path="pandits" element={<PanditsPage />} />
+        <Route path="devotees" element={<DevoteesPage />} />
+        <Route path="bookings" element={<BookingHistory />} />
+        <Route
+          path="map"
+          element={<MapView userLocations={userLocations} panditLocations={panditLocations} />}
+        />
 
-        {/* Optional: redirect any unknown routes to admin home */}
-        <Route path="*" element={<Navigate to="/admin/home" replace />} />
-      </Routes>
-    </Router>
+        {/* Redirect unknown /admin routes back to home */}
+        <Route path="*" element={<Navigate to="home" replace />} />
+      </Route>
+    </Routes>
   );
 }
 
