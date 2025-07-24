4427a573f0be1b069f77bb6b508a6e087333d973
shivamchoudhary15 <sc0626110@gmail.com>
2025-07-24 12:35:57 +0530

    Update AdminDashboard.js

--- pandit/src/pages/AdminDashboard.js
+++ pandit/src/pages/AdminDashboard.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import DashboardLayout from '../components/DashboardLayout';
-import Home from './Home1';
+import Home1 from './Home1';
 import PanditsPage from './PanditsPage';
 import DevoteesPage from './DevoteesPage';
 import BookingHistory from './BookingHistory';
@@ -14,9 +14,9 @@ function AdminDashboard({ userLocations = [], panditLocations = [] }) {
     <Routes>
       <Route path="/" element={<DashboardLayout />}>
         {/* Redirect /admin to /admin/home */}
-        <Route index element={<Navigate to="home" replace />} />
+        <Route index element={<Navigate to="home1" replace />} />
 
-        <Route path="home" element={<Home />} />
+        <Route path="home1" element={<Home1 />} />
         <Route path="pandits" element={<PanditsPage />} />
         <Route path="devotees" element={<DevoteesPage />} />
         <Route path="bookings" element={<BookingHistory />} />
@@ -25,8 +25,8 @@ function AdminDashboard({ userLocations = [], panditLocations = [] }) {
           element={<MapView userLocations={userLocations} panditLocations={panditLocations} />}
         />
 
-        {/* Redirect unknown /admin routes back to home */}
-        <Route path="*" element={<Navigate to="home" replace />} />
+        {/* Redirect unknown /admin routes back to home1 */}
+        <Route path="*" element={<Navigate to="home1" replace />} />
       </Route>
     </Routes>
   );
